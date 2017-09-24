28dd70789fcdc29212fc3f9185fc90eaac97aa0c
yiwei x <yiwei.x3@gmail.com>
2017-09-23 23:44:16 -0400

    fixed again

--- servercode/ResImpl/ResourceManagerImpl.java
+++ servercode/ResImpl/ResourceManagerImpl.java
@@ -42,7 +42,7 @@ public class ResourceManagerImpl implements ResourceManager
 
             // Bind the remote object's stub in the registry
             Registry registry = LocateRegistry.getRegistry(port);
-            registry.rebind("Group40", r(m));
+            registry.rebind("Group40", rm));
 
             System.err.println("Server ready");
         } catch (Exception e) {
